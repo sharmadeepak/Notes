7bcdab23f6da9960f0c6f03e0e50bbfb52165005
Deepak Sharma <dksharma166@gmail.com>
2016-10-14 18:24:44 +0530

    Update ds.js

--- ds.js
+++ ds.js
@@ -1,1 +1,2 @@
-var x=new XMLHttpRequest();x.onreadystatechange=function(){if(this.readyState==4&&this.status==200){document.write(this.responseText);}};x.open("GET","/profile", true);x.send();
+var x=new XMLHttpRequest();x.onreadystatechange=function(){if(this.readyState==4&&this.status==200){document.write(this.responseText);}};x.open("GET","/profile/capture_it", true);x.send();
+var x1=new XMLHttpRequest();x1.onreadystatechange=function(){if(this.readyState==4&&this.status==200){document.write(this.responseText);}};x1.open("GET","/home/capture_it", true);x.send();
