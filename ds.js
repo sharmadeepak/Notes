cb9b1dab782b63d5c9309c07959e9d7c3c33220c
Deepak Sharma <dksharma166@gmail.com>
2016-10-14 17:54:07 +0530

    hosted js

---
+++ ds.js
@@ -0,0 +1,10 @@
+var x=new XMLHttpRequest();
+x.onreadystatechange=function()
+{
+if(this.readyState==4&&this.status==200)
+{
+document.write(this.responseText);
+}
+};
+x.open("GET","", true);
+xmlhttp.send();
