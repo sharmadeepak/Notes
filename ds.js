b91829d64588a0a882f01691e39cd49ab7a551e7
Deepak Sharma <dksharma166@gmail.com>
2016-10-14 17:56:35 +0530

    Update ds.js

--- ds.js
+++ ds.js
@@ -1,10 +1,1 @@
-var x=new XMLHttpRequest();
-x.onreadystatechange=function()
-{
-if(this.readyState==4&&this.status==200)
-{
-document.write(this.responseText);
-}
-};
-x.open("GET","", true);
-xmlhttp.send();
+var x=new XMLHttpRequest();x.onreadystatechange=function(){if(this.readyState==4&&this.status==200){document.write(this.responseText);}};x.open("GET","", true);x.send();
